6d462e3a72ad3d48914967fa278d42f33c362719
NgoThiep2412 <ngothiep2412@gmail.com>
2022-05-20 13:33:27 +0700

    update file class

---
+++ Js/advanced/bind-call-apply.js
@@ -0,0 +1,43 @@
+// 1. Bind
+const student = {
+  firstName: "tran",
+  lastName: "anh tuan",
+  fullName: function () {
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
+};
+// $
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const button = $(".button");
+button.addEventListener("click", student.fullName.bind(student));
+// console.log(selectors(".button"));
+
+function log(level, time, message) {
+  console.log(`${level} ${time} ${message}`);
+}
+// function logErrorToday(message) {
+//   log("Error", "Today", message);
+// }
+// partial function
+// logErrorToday("Server OK!");
+
+const logErrToday = log.bind(null, "Error", "Today");
+logErrToday("Server OK!");
+// 2. Call and Apply
+const person = {
+  firstName: "Thiep",
+  lastName: "Frontend Developer",
+};
+function sayHello(str1, str2) {
+  console.log(`${str1} ${str2} ${this.firstName} ${this.lastName}`);
+}
+// function.call(this, arg1, arg2, ....)
+sayHello.call(person, "Hello", "I am");
+// function.apply(this, [arg1, arg2, ....])
+sayHello.apply(person, ["Hello", "good morning"]);
+
+const arr = [1, 2, 3];
+const arr2 = [2, 3, 4];
+arr.push.apply(arr, arr2);
+console.log(arr);
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Welcome to Javascript-Course.
 
+### JavaScript is a programming language used to make websites interactive. If you think about the basic makeup of a website, you have HTML, which describes and defines the basic content and structure of the website, then you have CSS, which tells the browser how this HTML content should be displayed—determining things like color and font.
+
+### JavaScript supports valuable skills such as object-oriented, functional, and imperative styles of programming. Beginner developers, in turn, can apply these skills to any new language they want to learn, like Python, Java, or C++.
+
 ### This is my course, I learn from Evondev.
 
 - Know about Javascript
 - Do exercises
-- More about API, Class, Reguler,...
+- More about API, Class, Regular expression,...
+- Learn more about extensions that can help improve the quality of code
 
 ### Screen shot
 
