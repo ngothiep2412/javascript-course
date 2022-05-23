a210e52148b4ccf64e36e0b64dde0c3f6b63bb4e
NgoThiep2412 <ngothiep2412@gmail.com>
2022-05-24 01:19:29 +0700

    update crud course with api

--- Js/api/placeholder.js
+++ Js/api/placeholder.js
@@ -1,31 +1,149 @@
-const course = {
-  image: "",
-  title: "",
-  author: "",
-  rating: "",
-  price: "",
-  bestSeller: "",
-  buyAmount: "",
-};
+// const { image, title, author, rating, price, bestSeller, buyAmount } = course;
+const endpoint = "http://localhost:3456/courses";
+const courseList = document.querySelector(".course-list");
+const formPost = document.querySelector(".form-post");
+const formSubmit = document.querySelector(".form-submit");
+let updateId = null;
+async function addNewCourse({
+  image,
+  title,
+  author,
+  rating,
+  price,
+  bestSeller,
+  buyAmount,
+}) {
+  await fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify({
+      image,
+      title,
+      author,
+      rating,
+      price,
+      bestSeller,
+      buyAmount,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+}
 
-// async function addPost(title, author) {
-//   const reponse = await fetch("http://localhost:3000/posts", {
-//     method: "POST",
-//     body: JSON.stringify({
-//       title,
-//       author,
-//     }),
-//     headers: {
-//       "Content-type": "application/json; charset=UTF-8",
-//     },
-//   });
-//   return reponse;
-// }
+async function updateCourse({
+  id,
+  image,
+  title,
+  author,
+  rating,
+  price,
+  bestSeller,
+  buyAmount,
+}) {
+  await fetch(`${endpoint}/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      image,
+      title,
+      author,
+      rating,
+      price,
+      bestSeller,
+      buyAmount,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+}
 
-// const formPost = document.querySelector(".form-post");
-// formPost.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   const title = this.elements["title"].value;
-//   const author = this.elements["author"].value;
-//   addPost(title, author);
-// });
+async function deleteCourse(id) {
+  await fetch(`${endpoint}/${id}`, {
+    method: "DELETE",
+  });
+}
+
+async function getSingleCourse(id) {
+  const response = await fetch(`${endpoint}/${id}`);
+  const data = response.json();
+  return data;
+}
+
+function readItem(item) {
+  const template = `<div class="course-item">
+  <div class="course-image">
+    <img src="${item.image}" alt="" />
+    <button class="course-edit" data-id="${
+      item.id
+    }"><i class="fa fa-pencil" style="pointer-events: none;"></i></button>
+    <button class="course-remove" data-id="${
+      item.id
+    }"><i class="fa fa-times"></i></button>
+  </div>
+  <div class="course-content">
+    <h3 class="course-title">
+      ${item.title}
+    </h3>
+    <div class="course-author">${item.author}</div>
+    <div class="course-meta">
+      <div class="course-rating">${item.rating}</div>
+      <div class="course-enroll">${item.buyAmount}</div>
+    </div>
+    ${
+      item.bestSeller ? `<div class="course-best-seller">Best Seller</div>` : ""
+    }   
+  </div>
+</div>`;
+  courseList.insertAdjacentHTML("beforeend", template);
+}
+
+async function getCourses() {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  courseList.innerHTML = "";
+  if (data.length > 0 && Array.isArray(data)) {
+    data.forEach((item) => readItem(item));
+  }
+}
+
+formPost.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const course = {
+    image: this.elements["image"].value,
+    title: this.elements["title"].value,
+    author: this.elements["author"].value,
+    rating: +this.elements["rating"].value,
+    price: +this.elements["price"].value,
+    bestSeller: this.elements["bestSeller"].checked,
+    buyAmount: +this.elements["buyAmount"].value,
+  };
+  updateId
+    ? await updateCourse({ id: updateId, ...course })
+    : await addNewCourse(course);
+  this.reset();
+  await getCourses();
+  updateId = null;
+  formSubmit.textContent = "Add course";
+});
+
+getCourses();
+
+courseList.addEventListener("click", async function (e) {
+  if (e.target.matches(".course-remove")) {
+    const id = +e.target.dataset.id;
+    await deleteCourse(id);
+    await getCourses();
+  } else if (e.target.matches(".course-edit")) {
+    const id = +e.target.dataset.id;
+    const data = await getSingleCourse(id);
+    formPost.elements["image"].value = data.image;
+    formPost.elements["title"].value = data.title;
+    formPost.elements["author"].value = data.author;
+    formPost.elements["rating"].value = data.rating;
+    formPost.elements["price"].value = data.price;
+    formPost.elements["bestSeller"].checked = data.bestSeller;
+    formPost.elements["buyAmount"].value = data.buyAmount;
+    formSubmit.textContent = "Update course";
+    updateId = id;
+  }
+});
