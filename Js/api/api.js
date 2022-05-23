0514f2ea5f86d17778dfe139b0b3541f6a182390
NgoThiep2412 <ngothiep2412@gmail.com>
2022-05-23 11:07:45 +0700

    update exercises api

--- Js/api/api.js
+++ Js/api/api.js
@@ -2,3 +2,20 @@
 // Application Programming Interface
 // https://api.github.com/users/evondev
 // JSON.parse(data)
+const endpoint = "https://api.github.com/users";
+const userEl = document.querySelector(".username");
+// fetch
+
+async function displayUser(username) {
+  userEl.textContent = "Loading ....";
+  const promise = await fetch(`${endpoint}/${username}`);
+  const data = await promise.json();
+  userEl.textContent = `${data.login}`;
+}
+function handleError() {
+  console.log("Something wrong with your api");
+  userEl.textContent = "No data found";
+}
+// displayUser("ngothiep2412");
+displayUser("asdasd").catch(handleError);
+// cors
