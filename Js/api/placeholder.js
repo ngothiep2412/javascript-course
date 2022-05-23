e21e381dc65012ad4ae816abae3c074abfaab7aa
NgoThiep2412 <ngothiep2412@gmail.com>
2022-05-23 22:13:49 +0700

    update db.json

---
+++ Js/api/placeholder.js
@@ -0,0 +1,31 @@
+const course = {
+  image: "",
+  title: "",
+  author: "",
+  rating: "",
+  price: "",
+  bestSeller: "",
+  buyAmount: "",
+};
+
+// async function addPost(title, author) {
+//   const reponse = await fetch("http://localhost:3000/posts", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       title,
+//       author,
+//     }),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   });
+//   return reponse;
+// }
+
+// const formPost = document.querySelector(".form-post");
+// formPost.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const title = this.elements["title"].value;
+//   const author = this.elements["author"].value;
+//   addPost(title, author);
+// });
